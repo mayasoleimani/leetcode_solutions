2e8b353118b9081cd832a3382111ee32f6d4cecf
Maya Soleimani <82066258+mayasoleimani@users.noreply.github.com>
2023-05-22 12:40:56 -0400

    Update leet_1480.py

--- leet_1480.py
+++ leet_1480.py
@@ -1,1 +1,16 @@
+class Solution(object):
+    def runningSum(self, nums):
+        runSum=list()
+        for x in range(0,len(nums)):
+            if x==0:                        #first index
+                runSum.append(nums[x])
+            elif x==len(nums)-1:            #last index
+                runSum.append(sum(nums))
+            else:                           #other indeces
+                runSum.append(sum(nums[:x+1]))
+        return runSum
 
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
