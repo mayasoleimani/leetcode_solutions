68764ccda26834a508d9e507c51580ccfa73e689
Maya Soleimani <82066258+mayasoleimani@users.noreply.github.com>
2023-05-25 14:39:10 -0400

    Update leet_724.py

--- leet_724.py
+++ leet_724.py
@@ -1,1 +1,22 @@
+class Solution(object):
+    def pivotIndex(self, nums):
 
+        total= sum(nums)
+        right_in=0
+        left_in=0
+        
+        for i in range(len(nums)):
+
+            right_in=total - nums[i] - left_in
+
+            if right_in == left_in:
+                print(i)
+                return i
+            else:
+                left_in=left_in+nums[i]
+        return -1
+
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
