68d91287b986baa20c51139ba162f2dcc02e77f6
Maya Soleimani <82066258+mayasoleimani@users.noreply.github.com>
2023-06-22 22:28:20 -0400

    Answer

--- leet_1431.py
+++ leet_1431.py
@@ -1,1 +1,20 @@
+class Solution(object):
+    def kidsWithCandies(self, candies, extraCandies):
 
+        returnList=[]
+        maxx=max(candies)
+
+        for i in candies:
+
+            if (i+extraCandies > maxx) or (i+extraCandies == maxx):
+                returnList.append(True)
+            else:
+                returnList.append(False)
+
+
+        return(returnList)
+        """
+        :type candies: List[int]
+        :type extraCandies: int
+        :rtype: List[bool]
+        """
