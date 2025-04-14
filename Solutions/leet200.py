3dc8f0e93abb90e8519b750ef16746770ff0674e
mayasoleimani <82066258+mayasoleimani@users.noreply.github.com>
2025-04-14 14:23:04 -0700

    update

--- Solutions/leet200.py
+++ Solutions/leet200.py
@@ -1,31 +1,23 @@
-grid = [
-  ["0","0","0","0","0"],
-  ["0","0","0","0","0"],
-  ["0","0","0","0","0"],
-  ["0","0","0","0","0"]
-]
-def dfs(grid, x, y,num):
-    #boundary/visited check
-    if x < 0 or x >= len(grid) or y < 0 or y >= len(grid[0]):
-        return 0
-    if grid[x][y] != '1':
-        return 0
-    #visited
-    grid[x][y] = -1
-    
-    dfs(grid, x-1, y,num) #down
-    dfs(grid, x+1, y,num) #up
-    dfs(grid, x, y-1,num) #left
-    dfs(grid, x, y+1,num) #right
-    return num+1
 
 def numIslands(grid):
+    def dfs(grid, x, y):
+        #boundary/visited check
+        if x < 0 or x >= len(grid) or y < 0 or y >= len(grid[0]):
+            return 0
+        if grid[x][y] != '1':
+            return 0
+        #visited
+        grid[x][y] = -1
+        
+        dfs(grid, x-1, y) #down
+        dfs(grid, x+1, y) #up
+        dfs(grid, x, y-1) #left
+        dfs(grid, x, y+1) #right
+
     num=0
     for i in range(len(grid)):
         for j in range(len(grid[0])):
             if grid[i][j] == '1':
-                num=dfs(grid, i, j,num)
+                dfs(grid,i,j)
+                num=+1
     return num
-
-
-numIslands(grid)
